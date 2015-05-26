84e82af6ca9b1014286d6eb4cd25af2b017a4cc4
feihuohua <feihuohua@163.com>
2015-05-26 14:12:51 +0800

    [wang]增加关于界面

---
+++ XEEnglish/ExpendCalss/Setting/MoreSetting/UserProblemsVC.h
@@ -0,0 +1,13 @@
+//
+//  UserProblemsVC.h
+//  XEEnglish
+//
+//  Created by houjing on 15/5/26.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface UserProblemsVC : BaseVC
+
+@end
---
+++ XEEnglish/ExpendCalss/Setting/MoreSetting/UserProtocolVC.h
@@ -0,0 +1,13 @@
+//
+//  UserProtocolVC.h
+//  XEEnglish
+//
+//  Created by houjing on 15/5/26.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface UserProtocolVC : BaseVC
+
+@end
