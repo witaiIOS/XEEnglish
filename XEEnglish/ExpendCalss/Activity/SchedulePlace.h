65f18d393d948b079622b8eedb077d8881c7d6c9
feihuohua <feihuohua@163.com>
2015-06-02 11:14:55 +0800

    [wang]增加场馆预定界面

---
+++ XEEnglish/ExpendCalss/Activity/SchedulePlace.h
@@ -0,0 +1,13 @@
+//
+//  SchedulePlace.h
+//  XEEnglish
+//
+//  Created by houjing on 15/6/2.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface SchedulePlace : BaseVC
+
+@end
