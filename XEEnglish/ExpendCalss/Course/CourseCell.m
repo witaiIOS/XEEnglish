939a965aad41f99820a7faabb2641846212ffee3
feihuohua <feihuohua@163.com>
2015-06-18 09:47:13 +0800

    [wang]修改课程cell中返回的状态

--- XEEnglish/ExpendCalss/Course/CourseCell.m
+++ XEEnglish/ExpendCalss/Course/CourseCell.m
@@ -17,7 +17,41 @@
     
     self.courseTitle.text = self.courseInfo[@"title"];
     self.courseTime.text = self.courseInfo[@"create_time"];
-    self.courseState.text = [NSString stringWithFormat:@"%@",self.courseInfo[@"is_signon"]];
+    NSString *signonStr =[NSString stringWithFormat:@"%@",self.courseInfo[@"is_signon"]];
+    
+    signonStr = [self judgeState:signonStr];
+    
+    self.courseState.text = signonStr;
+    //self.courseState.text = [NSString stringWithFormat:@"%@",self.courseInfo[@"is_signon"]];
+}
+
+- (NSString *)judgeState:(NSString *)signonStr{
+    
+    NSString *judgeState;
+    if ([signonStr isEqualToString:@"0"]) {
+        judgeState = @"等待上课";
+    }
+    else if ([signonStr isEqualToString:@"1"]){
+        judgeState = @"已上课";
+    }
+    else if ([signonStr isEqualToString:@"2"]){
+        judgeState = @"请假";
+    }
+    else if ([signonStr isEqualToString:@"3"]){
+        judgeState = @"缺课";
+    }
+    else if ([signonStr isEqualToString:@"4"]){
+        judgeState = @"延迟";
+    }
+    else if ([signonStr isEqualToString:@"5"]){
+        judgeState = @"暂停";
+    }
+    else{
+        judgeState = @"";
+    }
+    
+    return judgeState;
+
 }
 
 - (IBAction)leaveButtonClicked:(id)sender {
