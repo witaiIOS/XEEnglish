a4182e560990a3c7cc33981c9c5d00de76ea1e89
feihuohua <feihuohua@163.com>
2015-07-07 11:05:34 +0800

    [wang]小修改

--- AppConfig/AppConfig.h
+++ AppConfig/AppConfig.h
@@ -33,8 +33,8 @@
 
 
 #define XEEimageURLPrefix @"http://218.244.143.58:604/files/"
-//#define XEEWebURL @"http://218.244.143.58:604/ShaneAppWs.asmx?wsdl"
-#define XEEWebURL @"http://192.168.1.122:625/ShaneAppWs.asmx" //本地测试
+#define XEEWebURL @"http://218.244.143.58:604/ShaneAppWs.asmx?wsdl"
+//#define XEEWebURL @"http://192.168.1.122:625/ShaneAppWs.asmx" //本地测试
 //测试Token
 #define ceShiId                 @"17"
 #define ceShiToken              @"yEqHDenWZHANQ08W1WF93J+t+ELfkokN0dz9KsI8kALd/GXU1pEykMyOJupHiHATpELTmg8I+XWN1mKJb8Vr+g=="
