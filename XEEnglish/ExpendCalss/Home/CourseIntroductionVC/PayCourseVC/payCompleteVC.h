9a96d352cde8a310070aaaa67d0888a2f03643b9
feihuohua <feihuohua@163.com>
2015-07-08 15:07:45 +0800

    [wang]增加完成支付页面

---
+++ XEEnglish/ExpendCalss/Home/CourseIntroductionVC/PayCourseVC/payCompleteVC.h
@@ -0,0 +1,13 @@
+//
+//  payCompleteVC.h
+//  XEEnglish
+//
+//  Created by houjing on 15/7/8.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface payCompleteVC : BaseVC
+
+@end
