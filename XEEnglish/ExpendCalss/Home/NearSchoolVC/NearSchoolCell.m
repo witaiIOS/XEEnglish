9657d353210998adb7174e05ec82bc0392cd67cc
lionryin <710523800@qq.com>
2015-07-13 16:41:16 +0800

    [lx]修改distance

--- XEEnglish/ExpendCalss/Home/NearSchoolVC/NearSchoolCell.m
+++ XEEnglish/ExpendCalss/Home/NearSchoolVC/NearSchoolCell.m
@@ -14,10 +14,13 @@
     
     [super layoutSubviews];
     
+    NSLog(@"schoolInfoDic:%@",self.schoolInfoDic);
+    
     self.schoolNameLabel.text = self.schoolInfoDic[@"department"];
     //NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
     //NSNumber *dictence = self.schoolInfoDic[@"distance"];
-    self.schoolDistanceLabel.text = [NSString stringWithFormat:@"%@km",self.schoolInfoDic[@"distance"]];
+    NSString *dictence = self.schoolInfoDic[@"distance"];
+    self.schoolDistanceLabel.text = [NSString stringWithFormat:@"%.2fkm",[dictence floatValue]];
     [self.schoolImageView setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",XEEimageURLPrefix,self.schoolInfoDic[@"pic_url"]]] placeholderImage:[UIImage imageNamed:@"image_loading.png"]];
     self.schoolAddresslabel.text = [NSString stringWithFormat:@"校区地址：%@",self.schoolInfoDic[@"addr"]];
     //self.schoolAddresslabel.numberOfLines = 0;
