cf57de0abb8de8a6202d6366feef06ca57aa63e7
feihuohua <feihuohua@163.com>
2015-07-13 20:49:24 +0800

    [wang]修改积分兑换的bug

--- XEEnglish/ExpendCalss/Setting/Points/PointsRecordCell.m
+++ XEEnglish/ExpendCalss/Setting/Points/PointsRecordCell.m
@@ -13,9 +13,10 @@
 - (void)layoutSubviews{
     
     [super layoutSubviews];
+    //NSLog(@"info:%@",self.pointRecordInfoDic);
     
     self.pointExchange.text = self.pointRecordInfoDic[@"audit_statue_cn"];
-    self.pointExchangeType.text = self.pointRecordInfoDic[@"trade_type"];
+    self.pointExchangeType.text = self.pointRecordInfoDic[@"trade_type_cn"];
     self.pointRemainder.text = self.pointRecordInfoDic[@"remainder"];
     self.pointExchange.text = self.pointRecordInfoDic[@"number"];
     self.pointExchangeTime.text = self.pointRecordInfoDic[@"operate_time"];
