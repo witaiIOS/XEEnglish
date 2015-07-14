7d9346794a46e3a6ad88c3b6c38f364f2c65c99f
feihuohua <feihuohua@163.com>
2015-07-14 13:41:45 +0800

    [wang]增加我的预定课程

--- XEEnglish/ExpendCalss/Setting/Schedule/MyScheduleVC.m
+++ XEEnglish/ExpendCalss/Setting/Schedule/MyScheduleVC.m
@@ -9,6 +9,7 @@
 #import "MyScheduleVC.h"
 #import "LXSegmentViewThree.h"
 
+#import "ExpendRecordCell.h"
 #import "MyActivityCell.h"
 #import "MyBookSiteCell.h"
 
@@ -16,6 +17,9 @@
 
 @interface MyScheduleVC ()<UITableViewDataSource,UITableViewDelegate>
 @property (nonatomic, strong) LXSegmentViewThree *mySegmentView;
+
+@property (nonatomic, strong) UITableView *courseTableView;//我预定的课程
+@property (nonatomic, strong) NSMutableArray *courseArray;//
 
 @property (nonatomic, strong) UITableView *activityTableView;//我预定的活动。
 @property (nonatomic, strong) NSMutableArray *activityArray;
@@ -39,6 +43,9 @@
     
     self.automaticallyAdjustsScrollViewInsets = NO;
     
+    self.courseArray = [NSMutableArray array];
+    [self getMyCourseInfo];
+    
     self.activityArray = [NSMutableArray array];
     [self getMyActivityInfo];
     
@@ -56,6 +63,12 @@
     
     [self.view addSubview:self.mySegmentView];
     
+    self.courseTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight-64-44) style:UITableViewStyleGrouped];
+    self.courseTableView.backgroundColor = [UIColor clearColor];
+    self.courseTableView.dataSource = self;
+    self.courseTableView.delegate = self;
+    
+    [self.mySegmentView.mainScrollView addSubview:self.courseTableView];
     
     self.activityTableView = [[UITableView alloc] initWithFrame:CGRectMake(kScreenWidth, 0, kScreenWidth, kScreenHeight-64-44) style:UITableViewStyleGrouped];
     self.activityTableView.backgroundColor = [UIColor clearColor];
@@ -78,6 +91,34 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)getMyCourseInfo{
+    
+    NSDictionary *userDic = [[UserInfo sharedUser] getUserInfoDic];
+    NSDictionary *userInfoDic = userDic[uUserInfoKey];
+    
+    [self showHudWithMsg:@"载入中..."];
+    [[XeeService sharedInstance] getVOrderByParentIdWithParentId:userInfoDic[uUserId] andSort:@"" andOrder:@"" andPageSize:10 andPageIndex:1 andToken:userInfoDic[uUserToken] andBlock:^(NSDictionary *result, NSError *error) {
+        [self hideHud];
+        if (!error) {
+            NSNumber *isResult = result[@"result"];
+            //NSLog(@"result:%@",result);
+            if (isResult.integerValue == 0) {
+                NSDictionary *courseDic = result[@"resultInfo"];
+                
+                self.courseArray = courseDic[@"data"];
+                //NSLog(@"array:%@",self.courseArray);
+                [self.courseTableView reloadData];
+            }
+            else{
+                [UIFactory showAlert:@"未知错误"];
+            }
+        }else{
+            [UIFactory showAlert:@"网络错误"];
+        }
+    }];
+}
+
 
 - (void)getMyActivityInfo{
     NSDictionary *userDic = [[UserInfo sharedUser] getUserInfoDic];
@@ -145,7 +186,11 @@
 #pragma mark - UITableView DataSource
 - (NSInteger )numberOfSectionsInTableView:(UITableView *)tableView{
     
-    if (tableView == self.activityTableView) {
+    if (tableView == self.courseTableView) {
+        //NSLog(@"activityArray:%li",(unsigned long)self.activityArray.count);
+        return [self.courseArray count];
+    }
+    else if (tableView == self.activityTableView) {
         //NSLog(@"activityArray:%li",(unsigned long)self.activityArray.count);
         return [self.activityArray count];
     }
@@ -166,11 +211,28 @@
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *reuse1 = @"ExpendRecordVCCell";
     static NSString *reuse2 = @"MyActivityVCCell";
     static NSString *reuse3 = @"MyBookSiteVCCell";
     static NSString *reuse4 = @"MyCell";
     
-    if (tableView == self.activityTableView) {
+    if (tableView == self.courseTableView) {
+        
+        [tableView registerNib:[UINib nibWithNibName:@"ExpendRecordCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:reuse1];
+        
+        ExpendRecordCell *cell = [tableView dequeueReusableCellWithIdentifier:reuse1];
+        
+        if (cell == nil) {
+            cell = [[ExpendRecordCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuse1];
+        }
+        cell.cellEdge = 10;
+        cell.expendRecordInfoDic = self.courseArray[indexPath.section];
+        
+        return cell;
+        
+    }
+    
+    else if (tableView == self.activityTableView) {
         
         [tableView registerNib:[UINib nibWithNibName:@"MyActivityCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:reuse2];
         
@@ -215,7 +277,10 @@
 
 - (CGFloat )tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    if (tableView == self.activityTableView) {
+    if (tableView == self.courseTableView) {
+        return 190.0f;
+    }
+    else if (tableView == self.activityTableView) {
         return 280.0f;
     }
     else if (tableView == self.bookSiteTableView){
