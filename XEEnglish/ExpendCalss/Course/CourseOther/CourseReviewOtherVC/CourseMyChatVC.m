6fa7f1b855ca2d8fc61d1dfa29946670c8f60581
feihuohua <feihuohua@163.com>
2015-08-05 20:15:37 +0800

    [wang]聊天页增加刷新

--- XEEnglish/ExpendCalss/Course/CourseOther/CourseReviewOtherVC/CourseMyChatVC.m
+++ XEEnglish/ExpendCalss/Course/CourseOther/CourseReviewOtherVC/CourseMyChatVC.m
@@ -16,6 +16,9 @@
 
 @property (nonatomic, strong) UIView *footView;
 @property (nonatomic, strong) UITextField *chatTF;//聊天输入框
+
+@property (nonatomic, assign) NSInteger currentCommentPageIndex;//当前评论页
+@property (nonatomic, assign) NSInteger totalCOmmentPageIndex;//评论总页数
 @end
 
 @implementation CourseMyChatVC
@@ -23,6 +26,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"历史聊天";
+    
+    _currentCommentPageIndex = 1;
+    _totalCOmmentPageIndex = 0;
+    
+    [self setupRefresh:@"table"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,7 +46,7 @@
     self.commentArray = [NSMutableArray array];
     
     //网络请求评论数据
-    [self getCourseScheduleSignParentCommentWithWeb];
+    //[self getCourseScheduleSignParentCommentWithWeb];
     
     //添加键盘上的done按钮
     [self addKeyboardDone];
@@ -55,7 +63,7 @@
     
     //[self moveToTableViewBottom];
 }
-
+//滚动到TableView的底部
 - (void)moveToTableViewBottom{
     
     if (self.tableView.contentSize.height > self.tableView.frame.size.height)
@@ -146,7 +154,13 @@
             
             NSNumber *isResult = result[@"result"];
             if (isResult.integerValue == 0) {
-                [self getCourseScheduleSignParentCommentWithWeb];
+                //发送聊天内容重新刷新页面
+                //[self getCourseScheduleSignParentCommentWithWeb];
+                _currentCommentPageIndex = 1;
+                _totalCOmmentPageIndex = 0;
+                
+                [self setupRefresh:@"table"];
+                
                 [UIFactory showAlert:result[@"resultInfo"]];
             }
             else{
@@ -161,33 +175,144 @@
 }
 
 #pragma mark - Web
-- (void)getCourseScheduleSignParentCommentWithWeb {
+//- (void)getCourseScheduleSignParentCommentWithWeb {
+//
+//    NSDictionary *userDic = [[UserInfo sharedUser] getUserInfoDic];
+//    NSDictionary *userInfoDic = userDic[uUserInfoKey];
+//
+//    [self showHudWithMsg:@"加载中..."];
+//    [[XeeService sharedInstance] getCourseScheduleSignParentCommentWithParentId:userInfoDic[uUserId] andCourseScheduleId:@"" andPageSize:10 andPageIndex:1 andSignonId:self.courseInfoDic[@"signon_id"] andToken:userInfoDic[uUserToken] andBlock:^(NSDictionary *result, NSError *error) {
+//        [self hideHud];
+//        if (!error) {
+//            //NSLog(@"result:%@",result);
+//            NSNumber *isResult = result[@"result"];
+//            if (isResult.integerValue == 0) {
+//                //查询这节课老师和家长的评论，传值signon_id课表签到id，course_schedule_id不传值。
+//                NSDictionary *commentDic = result[@"resultInfo"];
+//                self.commentArray = commentDic[@"data"];
+//                //NSLog(@"array:%@",self.myCommentArray);
+//                [self.tableView reloadData];
+//                //请求数据成功后，滚动到TableView的底部
+//                [self performSelector:@selector(moveToTableViewBottom) withObject:nil afterDelay:0.3];//滚动课程。
+//            }else{
+//                [UIFactory showAlert:result[@"resultInfo"]];
+//            }
+//        }else{
+//            [UIFactory showAlert:@"网络错误"];
+//        }
+//
+//    }];
+//}
 
+- (void)getCourseScheduleSignParentCommentWithPageIndex:(NSInteger)pageIndex WithBlock:(void (^)(NSDictionary *result, NSError *error))block {
+    
     NSDictionary *userDic = [[UserInfo sharedUser] getUserInfoDic];
     NSDictionary *userInfoDic = userDic[uUserInfoKey];
+    
+    //[self showHudWithMsg:@"加载中..."];
+    [[XeeService sharedInstance] getCourseScheduleSignParentCommentWithParentId:userInfoDic[uUserId] andCourseScheduleId:@"" andPageSize:10 andPageIndex:1 andSignonId:self.courseInfoDic[@"signon_id"] andToken:userInfoDic[uUserToken] andBlock:block];
+}
 
-    [self showHudWithMsg:@"加载中..."];
-    [[XeeService sharedInstance] getCourseScheduleSignParentCommentWithParentId:userInfoDic[uUserId] andCourseScheduleId:@"" andPageSize:10 andPageIndex:1 andSignonId:self.courseInfoDic[@"signon_id"] andToken:userInfoDic[uUserToken] andBlock:^(NSDictionary *result, NSError *error) {
-        [self hideHud];
+
+#pragma mark -
+#pragma mark - MJRefresh
+/**
+ *  集成刷新控件
+ */
+- (void)setupRefresh:(NSString *)dateKey
+{
+    
+    // 1.下拉刷新(进入刷新状态就会调用self的headerRereshing)
+    // dateKey用于存储刷新时间，可以保证不同界面拥有不同的刷新时间
+    [_tableView addHeaderWithTarget:self action:@selector(headerRereshing) dateKey:dateKey];
+    //#warning 自动刷新(一进入程序就下拉刷新)
+    [_tableView headerBeginRefreshing];
+    
+    // 2.上拉加载更多(进入刷新状态就会调用self的footerRereshing)
+    [_tableView addFooterWithTarget:self action:@selector(footerRereshing)];
+    
+    // 设置文字(也可以不设置,默认的文字在MJRefreshConst中修改)
+    _tableView.headerPullToRefreshText = @"下拉可以刷新";
+    _tableView.headerReleaseToRefreshText = @"松开马上刷新";
+    _tableView.headerRefreshingText = @"正在努力帮您刷新中,不客气";
+    
+    _tableView.footerPullToRefreshText = @"上拉可以加载更多数据";
+    _tableView.footerReleaseToRefreshText = @"松开马上加载更多数据";
+    _tableView.footerRefreshingText = @"正在努力帮您加载中,不客气";
+    
+}
+
+- (void)headerRereshing{
+    self.currentCommentPageIndex = 1;
+    
+    [self getCourseScheduleSignParentCommentWithPageIndex:_currentCommentPageIndex WithBlock:^(NSDictionary *result, NSError *error) {
+        [self.tableView headerEndRefreshing];
+        
         if (!error) {
             //NSLog(@"result:%@",result);
             NSNumber *isResult = result[@"result"];
             if (isResult.integerValue == 0) {
-                //查询这节课老师和家长的评论，传值signon_id课表签到id，course_schedule_id不传值。
-                NSDictionary *commentDic = result[@"resultInfo"];
-                self.commentArray = commentDic[@"data"];
-                //NSLog(@"array:%@",self.myCommentArray);
+                NSDictionary *commentInfo = result[@"resultInfo"];
+                
+                NSMutableArray *array = [NSMutableArray array];
+                [array addObjectsFromArray:commentInfo[@"data"]];
+                
+                self.commentArray = array;
                 [self.tableView reloadData];
                 
+                //请求数据成功后，滚动到TableView的底部
                 [self performSelector:@selector(moveToTableViewBottom) withObject:nil afterDelay:0.3];//滚动课程。
+                
+                NSNumber *totalNum = commentInfo[@"totalPage"];
+                if (totalNum) {
+                    self.totalCOmmentPageIndex = totalNum.integerValue;
+                }
             }else{
-                [UIFactory showAlert:result[@"resultInfo"]];
+                [self showHudOnlyMsg:@"未知错误"];
             }
         }else{
-            [UIFactory showAlert:@"网络错误"];
+            [self showHudOnlyMsg:@"网络错误"];
         }
-
     }];
+}
+
+- (void)footerRereshing{
+    
+    if (_currentCommentPageIndex < _totalCOmmentPageIndex) {
+        _currentCommentPageIndex++;
+        
+        [self getCourseScheduleSignParentCommentWithPageIndex:_currentCommentPageIndex WithBlock:^(NSDictionary *result, NSError *error) {
+            [self.tableView footerEndRefreshing];
+            
+            if (!error) {
+                //NSLog(@"result:%@",result);
+                NSNumber *isResult = result[@"result"];
+                if (isResult.integerValue == 0) {
+                    NSDictionary *commentInfo = result[@"resultInfo"];
+                    
+                    [self.commentArray addObjectsFromArray:commentInfo[@"data"]];
+                    
+                    [self.tableView reloadData];
+                    
+                    //请求数据成功后，滚动到TableView的底部
+                    [self performSelector:@selector(moveToTableViewBottom) withObject:nil afterDelay:0.3];//滚动课程。
+                    
+                    NSNumber *totalNum = commentInfo[@"totalPage"];
+                    if (totalNum) {
+                        self.totalCOmmentPageIndex = totalNum.integerValue;
+                    }
+                }else{
+                    [self showHudOnlyMsg:@"未知错误"];
+                }
+            }else{
+                [self showHudOnlyMsg:@"网络错误"];
+            }
+        }];
+    }else{
+        [self.tableView footerEndRefreshing];
+        [self showHudOnlyMsg:@"已全部加载完"];
+    }
+    
 }
 
 //泡泡文本
